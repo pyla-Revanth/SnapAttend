7c6986e4a89638703344cb9bbe49d19cb71888ab
pyla-Revanth <revanthpyla19@gmail.com>
2026-07-30 13:39:52 +0530

    refactor: organize React frontend structure

---
+++ frontend/src/pages/Student/index.jsx
@@ -0,0 +1,5 @@
+function Student() {
+  return <h1>Student Page</h1>;
+}
+
+export default Student;
---
+++ frontend/src/pages/Teacher/index.jsx
@@ -0,0 +1,5 @@
+function Teacher() {
+  return <h1>Teacher Page</h1>;
+}
+
+export default Teacher;
