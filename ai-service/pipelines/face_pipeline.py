acb3e8e9913566ce331eef8fe9643172f5abfd59
pyla-Revanth <revanthpyla19@gmail.com>
2026-08-10 18:52:42 +0530

    feat(ai): add dlib face recognition models

--- ai-service/pipelines/face_pipeline.py
+++ ai-service/pipelines/face_pipeline.py
@@ -4,3 +4,16 @@ import face_recognition_models
 
 from sklearn.svm import SVC
 
+def load_dlib_models():
+
+    detector = dlib.get_frontal_face_detector()
+
+    shape = dlib.shape_predictor(
+        face_recognition_models.pose_predictor_model_location()
+    )
+
+    facerec = dlib.face_recognition_model_v1(
+        face_recognition_models.face_recognition_model_location()
+    )
+    
+    return detector, shape, facerec
