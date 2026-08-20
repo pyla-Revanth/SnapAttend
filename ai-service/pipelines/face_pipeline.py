8d74d50986f4b1bb2d383e1488655ffedc28b691
pyla-Revanth <revanthpyla19@gmail.com>
2026-08-20 22:08:35 +0530

    feat(ai): add face embedding verification

--- ai-service/pipelines/face_pipeline.py
+++ ai-service/pipelines/face_pipeline.py
@@ -25,19 +25,30 @@ def get_face_embeddings(image_np):
 
     faces = detector(image_np,1)
 
+    # [<dlib.rectangle object at 0x7f8b1c2a3d90>, <dlib.rectangle object at 0x7f8b1c2a3e50>]
+    # faces[0] : (120, 50, 320, 250)
+    # faces[1] : (450, 100, 600, 250)
+    #upscale 2x
+
     encodings = []
 
     for face in faces:
 
         shape = sp(image_np, face)
 
+        # <dlib.full_object_detection object at 0x...>
+        # (68 facial landmarks)
+        # p = shape.part(i) -> (p.x, p.y) coordinate
+
         face_decriptor = facerec.compute_face_descriptor(
             image_np,
             shape,
             1
         )
+        # face_decriptor -> (128,) array list
 
         encodings.append(np.array(face_decriptor))
+        # encodings -> [(128,), (128,), ...]
 
     return encodings
 
@@ -75,12 +86,14 @@ def train_classifier(X, y):
     clf.fit(X, y)
 
     return clf
-    
 
 
-def predict_faces(class_image_np, clf):
+def predict_faces(class_image_np, clf, X, y):
 
     if clf is None:
+        return []
+
+    if len(X) == 0 or len(y) == 0:
         return []
 
     encodings = get_face_embeddings(class_image_np)
@@ -90,10 +103,26 @@ def predict_faces(class_image_np, clf):
 
     detected_students = []
 
+    resemblance_threshold = 0.6
+
     for encoding in encodings:
 
         predicted_id = clf.predict([encoding])[0]
+        # List of predicted IDs so use [0] to get the first one
 
-        detected_students.append(int(predicted_id))
+        predicted_id = int(predicted_id)
+
+        student_index = y.index(predicted_id)
+
+        student_embedding = X[student_index]
+
+        best_match_score = np.linalg.norm(
+            student_embedding - encoding
+        )
+
+        if best_match_score <= resemblance_threshold:
+
+            detected_students.append(predicted_id)
 
     return detected_students
+
